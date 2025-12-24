9f02801ab2b4f11f243267c637bf803b434f09ad
Abhishek-snaatak <abhishek.bisht.snaatak@mygurukulam.co>
2025-12-24 08:48:56 +0530

    Update SyntaxCheck.groovy

--- src/org/devops/ansible/SyntaxCheck.groovy
+++ src/org/devops/ansible/SyntaxCheck.groovy
@@ -3,14 +3,13 @@ package org.devops.ansible
 class SyntaxCheck {
     static void run(Map config) {
         def script = config.script
-        def role = config.role
-
-        // Dynamically use role name if you want to target specific role
-        def playbookPath = "tests/test.yml"
-        def inventoryPath = "tests/inventory/dev.ini"
 
         script.sh """
-            ansible-playbook -i ${inventoryPath} ${playbookPath} --syntax-check
+        ANSIBLE_ROLES_PATH=roles \
+        ansible-playbook \
+          -i tests/inventory/dev.ini \
+          tests/test.yml \
+          --syntax-check
         """
     }
 }
