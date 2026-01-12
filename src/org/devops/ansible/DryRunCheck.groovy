575ea1895b0089e12ec4e035a294a5e284163fc8
Abhishek-snaatak <abhishek.bisht.snaatak@mygurukulam.co>
2026-01-12 08:04:50 +0530

    Update DryRunCheck.groovy

--- src/org/devops/ansible/DryRunCheck.groovy
+++ src/org/devops/ansible/DryRunCheck.groovy
@@ -2,8 +2,9 @@ package org.devops.ansible
 
 class DryRunCheck {
 
-    def run(steps, String playbook, String inventory) {
+    def run(steps, String playbook, String inventory, String rolesPath) {
         steps.sh """
+        ANSIBLE_ROLES_PATH=${rolesPath} \
         ansible-playbook \
           -i ${inventory} \
           ${playbook} \
