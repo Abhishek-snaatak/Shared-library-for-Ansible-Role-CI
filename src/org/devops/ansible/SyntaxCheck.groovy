ffc21d4a9c0478eca3e01c04b5f6a02235b36929
Abhishek-snaatak <abhishek.bisht.snaatak@mygurukulam.co>
2026-01-12 08:03:09 +0530

    Update SyntaxCheck.groovy

--- src/org/devops/ansible/SyntaxCheck.groovy
+++ src/org/devops/ansible/SyntaxCheck.groovy
@@ -2,9 +2,9 @@ package org.devops.ansible
 
 class SyntaxCheck {
 
-    def run(steps, String inventory, String playbook) {
+    def run(steps, String inventory, String playbook, String rolesPath) {
         steps.sh """
-        ANSIBLE_ROLES_PATH=roles \
+        ANSIBLE_ROLES_PATH=${rolesPath} \
         ansible-playbook \
           -i ${inventory} \
           ${playbook} \
