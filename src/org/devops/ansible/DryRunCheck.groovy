e9b896656471f92367d5ac3484de3c4679bccf0b
Abhishek-snaatak <abhishek.bisht.snaatak@mygurukulam.co>
2026-01-12 07:56:37 +0530

    Update DryRunCheck.groovy

--- src/org/devops/ansible/DryRunCheck.groovy
+++ src/org/devops/ansible/DryRunCheck.groovy
@@ -2,9 +2,7 @@ package org.devops.ansible
 
 class DryRunCheck {
 
-    def run(steps,
-         {
-
+    def run(steps, String playbook, String inventory) {
         steps.sh """
         ansible-playbook \
           -i ${inventory} \
